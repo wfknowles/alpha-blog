e6bcbbfb527bea9580b7fbcf92e6e2c1f09052ea
Rails Udemy Course <wfknowles@gmail.com>
2017-03-29 17:49:25 +0000

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
+    
     def index
         @articles = Article.all
     end
@@ -8,7 +11,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     
@@ -23,7 +26,7 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was succesfully updated"
             redirect_to article_path(@article)
@@ -35,18 +38,21 @@ class ArticlesController < ApplicationController
     
     
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         flash[:notice] = "Article was succesfully deleted!"
         redirect_to articles_path
     end
     
     private
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+        def set_article
+            @article = Article.find(params[:id])
+        end
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
 end
