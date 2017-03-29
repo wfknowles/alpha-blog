6f12dfd22b8826b52b6c1ed65d8b9e9d596f142b
Rails Udemy Course <wfknowles@gmail.com>
2017-03-29 16:08:33 +0000

    Saftey Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        render plain: params[:article].inspect
+    end
+    
+end
